5d2310f6bef0d064f98f7684c91a7e1b80a4d63c
BDPeppers <peppersbre@yahoo.com>
2020-05-18 14:02:17 -0400

    Updates

---
+++ precache-manifest.b467fa103e268b9de726af7cc9b1f000.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32dedbf0386f435b8f0aac4ed63035e7",
+    "url": "/katianastudios/index.html"
+  },
+  {
+    "revision": "84279a8173f949c71cfe",
+    "url": "/katianastudios/static/css/2.616d3657.chunk.css"
+  },
+  {
+    "revision": "9fb32fa446f70cc5afca",
+    "url": "/katianastudios/static/css/main.764ac2ce.chunk.css"
+  },
+  {
+    "revision": "84279a8173f949c71cfe",
+    "url": "/katianastudios/static/js/2.890f9c85.chunk.js"
+  },
+  {
+    "revision": "6a413fbe6ac0a3bffb10654948246cdf",
+    "url": "/katianastudios/static/js/2.890f9c85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fb32fa446f70cc5afca",
+    "url": "/katianastudios/static/js/main.36ff47ec.chunk.js"
+  },
+  {
+    "revision": "a24fff75b46bdcbbfc64",
+    "url": "/katianastudios/static/js/runtime-main.6c248946.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/katianastudios/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/katianastudios/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "cd84e77f21362fbebcdd0d171c884136",
+    "url": "/katianastudios/static/media/blush.cd84e77f.PNG"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/katianastudios/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/katianastudios/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/katianastudios/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/katianastudios/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/katianastudios/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/katianastudios/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/katianastudios/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/katianastudios/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/katianastudios/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/katianastudios/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/katianastudios/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/katianastudios/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/katianastudios/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/katianastudios/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/katianastudios/static/media/fa-solid-900.ec763292.svg"
+  }
+]);
